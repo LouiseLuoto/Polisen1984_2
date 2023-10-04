54e1c740e4d8d642e2a503f013d49835cfd5b778
Louise <louiseluoto@hotmail.com>
2023-10-04 11:22:35 +0200

    skapar namespace polis

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Police
+{
+    internal class Program
+    {
+        static void Main ()
+        {
+
+        }
+    }
+}
