4c5c701a4bbdccff00be1343e3e60ee5a9f261f3
Louise <louiseluoto@hotmail.com>
2023-10-04 11:50:51 +0200

    skapat lista med poliser

--- Program.cs
+++ Program.cs
@@ -1,11 +1,27 @@
 ﻿using System;
 namespace Police
 {
+    class Police
+    {
+        public string name;
+    }
     internal class Program
     {
         static void Main ()
         {
-
+            List<Police> polices = new List <Police>();
+            Police police = new Police();
+            police.name = "Bengt Karlsson";
+            polices.Add(police);
+            Police police2 = new Police();
+            police.name = "Orvar Pettersson";
+            polices.Add(police); 
+            Police police3 = new Police();
+            police.name = "Ylva Jonsson";
+            polices.Add(police);
+            Police police4 = new Police();
+            police.name = "Felicia Mellgren";
+            polices.Add(police);
         }
     }
 }
